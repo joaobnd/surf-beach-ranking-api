95c41eb9d7df48b50c137ec49a6996289f63fbf8
joao.martins <joao.martins@ccc.ufcg.edu.br>
2023-12-06 16:39:14 -0300

    creating api client

--- src/server.ts
+++ src/server.ts
@@ -5,29 +5,26 @@ import { Server } from '@overnightjs/core';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express-serve-static-core';
 
-
 export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
 
-    constructor(private port = 3000) {
-        super();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json())
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController])
-    }
-
-    public getApp(): Application {
-        return this.app;
-    }
-
+  public getApp(): Application {
+    return this.app;
+  }
 }
